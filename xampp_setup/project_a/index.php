f50cc1c040d5b6134e70ff93aeef6ad580fe8e9e
Ralph Reid <beresfordjunior@me.com>
2014-02-18 17:47:16 +0000

    initial commit

---
+++ xampp_setup/project_a/index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+    <h1>Welcome to Foo Site</h1>
+
+    <p><?php echo 'Here you can put dynamic content.'; ?></p>
+
+</body>
+</html>
---
+++ xampp_setup/project_b/index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+    <h1>Welcome to Bar Site</h1> <!-- change the header to differentiate them -->
+
+    <p><?php echo 'Here you can put dynamic content.'; ?></p>
+
+</body>
+</html>
